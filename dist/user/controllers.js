fc49bc5ebbb0de2082946304bd87348c125efcac
MACBOOK <macbook@MacBook-Air-de-MACBOOK.local>
2025-09-01 21:35:52 +0000

    save les enregistrements

---
+++ dist/user/controllers.js
@@ -0,0 +1,18 @@
+import { Router } from "express";
+export const UserRouter = Router();
+UserRouter.get("/users", (request, response) => {
+    response.send("liste des utilisateurs");
+});
+UserRouter.get("/users/:id", (request, response) => {
+    response.send(` utilisateur ${request.params.id}`);
+});
+UserRouter.post("/users", (request, response) => {
+    response.send(` utilisateur:
+        
+         ${request.body.firstname}
+         ${request.body.lastname}
+         ${request.body.email}
+         ${request.body.password}
+
+         `);
+});
---
+++ src/main.ts
@@ -0,0 +1,14 @@
+import EXPRESS from "express";
+import {UserRouter} from "@src/user/controllers";
+
+
+const application = EXPRESS()
+const port = 3000
+
+application.use(EXPRESS.urlencoded({}))
+application.use(UserRouter)
+
+
+application.listen(port,()=> console.log(`connecter sur http://localhost:${port}`))
+
+
