3146d8be9a2da9400b06bb201471faf3994d8a34
Aeysha2 <ndaicha27@gmail.com>
2025-09-09 21:38:36 +0000

    handler find all and create task

--- src/task/controllers.ts
+++ src/task/controllers.ts
@@ -1,19 +1,27 @@
 import { Router } from "express";
+import { PrismaClient,$Enums } from "@prisma/client";
 
 export const TaskRouter =  Router()
+const prisma = new PrismaClient 
 
 TaskRouter
-.get("/",(request,response)=> {
-    
-    response.send("liste des taches")
+.get("/",async(request,response)=> {
+      const tasks = await prisma.task.findMany()
+    response.json({tasks})
 })
 
-.get("/:id",(request,response)=> {
+.get("/:id",async (request,response)=> {
+
     
     response.send(` tache ${request.params.id}`)
 })
 
-.post("/",(request,response)=> {
+.post("/",async(request,response)=> {
+    const {
+        title,
+        description
+    }= request.body
+    await prisma.task.create({data:{title,description,status:$Enums.TaskStatus.PENDING}})
     
     response.send(` tache:
         
