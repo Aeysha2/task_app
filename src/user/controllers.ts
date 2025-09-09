6c8245e8a5e710a12c69caaf1899f068416148b9
Your Name <you@example.com>
2025-09-09 21:44:16 +0200

    add prisma config

--- src/user/controllers.ts
+++ src/user/controllers.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
+import { PrismaClient } from "@prisma/client";
 
 export const UserRouter =  Router()
-
+const prisma = new PrismaClient()
 UserRouter
-.get("/",(request,response)=> {
-    
-    response.send("liste des utilisateurs")
+.get("/",async (request,response)=> {
+    const users = await prisma.user.findMany()
+    response.json({users})
 })
 
 .get("/:id",(request,response)=> {
@@ -13,7 +14,14 @@ UserRouter
     response.send(` utilisateur ${request.params.id}`)
 })
 
-.post("/",(request,response)=> {
+.post("/", async (request,response)=> {
+    const {
+        email,
+        firstname,
+        lastname,
+        password
+    } = request.body
+    await prisma.user.create({data:{email,firstname,lastname,password}})
     
     response.send(` utilisateur:
         
