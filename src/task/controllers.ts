f3e408932dcf9aa0dbae45c90f26322ee3f83720
Your Name <you@example.com>
2025-09-15 23:37:28 +0200

    add starting task feature

--- src/task/controllers.ts
+++ src/task/controllers.ts
@@ -1,34 +1,48 @@
 import { Router } from "express";
-import { PrismaClient,$Enums } from "@prisma/client";
+import { PrismaClient, $Enums } from "@prisma/client";
 
-export const TaskRouter =  Router()
-const prisma = new PrismaClient 
+export const TaskRouter = Router();
+const prisma = new PrismaClient();
 
-TaskRouter
-.get("/",async(request,response)=> {
-      const tasks = await prisma.task.findMany()
-    response.json({tasks})
+TaskRouter.get("/", async (request, response) => {
+  const tasks = await prisma.task.findMany();
+  response.json({ tasks });
 })
 
-.get("/:id",async (request,response)=> {
+  .get("/:id", async (request, response) => {
+    const task = await prisma.task.findUnique({
+      where: { id: request.params.id },
+    });
+    response.json({ task });
+  })
 
-    
-    response.send(` tache ${request.params.id}`)
-})
+  .patch("/starting/:id", async (request, response) => {
+    try {
+      const taskID = request.params.id;
+      const task = await prisma.task.findUnique({ where: { id: taskID } });
+      if (!task) throw new Error(`Tâche non trouvée avec cet ID. ${taskID}`);
+      const taskUpdated = await prisma.task.update({
+        where: { id: taskID },
+        data: { status: $Enums.TaskStatus.STARTING },
+      });
 
-.post("/",async(request,response)=> {
-    const {
-        title,
-        description
-    }= request.body
-    await prisma.task.create({data:{title,description,status:$Enums.TaskStatus.PENDING}})
-    
+      response.json({ taskUpdated });
+    } catch (error: any) {
+      response.status(404).json({ message: error.message });
+    }
+  })
+
+  .post("/", async (request, response) => {
+    const { title, description } = request.body;
+    await prisma.task.create({
+      data: { title, description, status: $Enums.TaskStatus.PENDING },
+    });
+
     response.send(` tache:
         
          ${request.body.title}
          ${request.body.description}
          
 
-         `)
-    
-})
+         `);
+  });
