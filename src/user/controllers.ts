c7095a8c1116dfb94a908f54827aeadf90762ae0
Your Name <you@example.com>
2025-09-22 23:08:44 +0200

    add update user feature

--- src/user/controllers.ts
+++ src/user/controllers.ts
@@ -1,36 +1,40 @@
 import { Router } from "express";
 import { PrismaClient } from "@prisma/client";
 
-export const UserRouter =  Router()
-const prisma = new PrismaClient()
-UserRouter
-.get("/",async (request,response)=> {
-    const users = await prisma.user.findMany()
-    response.json({users})
+export const UserRouter = Router();
+const prisma = new PrismaClient();
+UserRouter.get("/", async (request, response) => {
+  const users = await prisma.user.findMany();
+  response.json({ users });
 })
 
-.get("/:id",async (request,response)=> {
-    
-    const users = await prisma.user.findUnique({where:{id:request.params.id}})
-    response.json({users})
-})
+  .get("/:id", async (request, response) => {
+    const user = await prisma.user.findUnique({
+      where: { id: request.params.id },
+    });
+    response.json({ user });
+  })
 
-.post("/", async (request,response)=> {
-    const {
-        email,
-        firstname,
-        lastname,
-        password
-    } = request.body
-    await prisma.user.create({data:{email,firstname,lastname,password}})
+  .put("/:id", async (request, response) => {
+    const userID =request.params.id
+    const user = await prisma.user.findUnique({
+      where: { id: request.params.id },
+    });
+    if (!user) throw new Error(`Utilisateur non trouvé avec cet ID. ${userID}`);
+    const { email, firstname, lastname } = request.body;
     
-    response.send(` utilisateur:
-        
-         ${request.body.firstname}
-         ${request.body.lastname}
-         ${request.body.email}
-         ${request.body.password}
+    const userUpdated = await prisma.user.update({
+            where: { id: userID },
+            data: { firstname,lastname,email },
+          });    
+    response.json({ user });
+  })
+  
 
-         `)
-    
-})
+  .post("/", async (request, response) => {
+    const { email, firstname, lastname, password } = request.body;
+    const user = await prisma.user.create({
+      data: { email, firstname, lastname, password },
+    });
+    response.json({ user });
+  });
