05c0d41b3a6690ba0982d8662ad4822a858425f9
Your Name <you@example.com>
2025-09-25 23:33:48 +0200

    add handlers cors authorization

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
 import EXPRESS from "express";
 import {UserRouter} from "./user/controllers.js";
+import cors from "cors"
 import {TaskRouter} from "./task/controllers.js";
 
 
 const application = EXPRESS()
 const port = 3000
-
+application.use(cors())
 application.use(EXPRESS.urlencoded({}))
 application.use("/users",UserRouter)
 application.use("/tasks",TaskRouter)
