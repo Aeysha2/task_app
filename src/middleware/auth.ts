a88190dc14185b13bd9d8a284f2090834f01f503
= <yacoukeita0212@gmail.com>
2025-10-06 21:37:38 +0000

    WIP:handler auth feature

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,5 +1,18 @@
-export const Auth = (request:any,response:any,next:any)=>{
-    console.log("token",request.headers) 
+import jwt from "jsonwebtoken";
 
-    next()
-} 
+export const Auth = (request: any, response: any, next: any) => {
+  try {
+    const authorization = request.headers.authorization;
+    if (!authorization) throw new Error("Connecter vous svp!");
+    const [type, token] = authorization.split(" ");
+
+   const  decodeToken = jwt.decode(token)
+    
+    console.log(decodeToken);
+    
+
+    next();
+  } catch (error:any) {
+    response.status(403).json({message:error.message})
+  }
+};
