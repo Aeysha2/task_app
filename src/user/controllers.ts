2eb7b67dbd2ecc708498f8ba74c5331278d5011c
Your Name <you@example.com>
2025-10-09 23:35:16 +0200

    FIX: Handler route /me issue for authenticate user

--- src/user/controllers.ts
+++ src/user/controllers.ts
@@ -1,51 +1,59 @@
 import { Router } from "express";
-import { createUser, findAll, findByID, loginUser, updateByID,forgotPassword } from "./service.js";
+import {
+  createUser,
+  findAll,
+  findByID,
+  loginUser,
+  updateByID,
+  forgotPassword,
+} from "./service.js";
 import { generateToken } from "../utils/jwt.js";
 import { Auth } from "../middleware/auth.js";
 
 export const UserRouter = Router();
-UserRouter.get("/", Auth ,async (request:any, response:any) => {
-    response.json({ user:await findAll() });
-} )
+UserRouter.get("/", Auth, async (request: any, response: any) => {
+  response.json({ user: await findAll() });
+})
 
-  .get("/:id", Auth ,async (request, response) => {
-    response.json({ user: await findByID(request.params.id) });
-  })
-  
-  .get("/me", Auth ,async (request:any, response) => {
+  .get("/me",Auth, async (request: any, response: any) => {
     response.json({ user: await findByID(request.userID) });
   })
 
-  .get("/forgotPassword/:email", Auth ,async (request, response) => {
+  .get("/:id", Auth, async (request, response) => {
+    response.json({ user: await findByID(request.params.id) });
+  })
+
+  .get("/forgotPassword/:email", Auth, async (request, response) => {
     try {
-      await forgotPassword(request.params.email)
-    response.json({ 
-      message: `Verifier votre email (${request.params.email}), nous vous avons envoyé un lien de renitialisation`});
+      await forgotPassword(request.params.email);
+      response.json({
+        message: `Verifier votre email (${request.params.email}), nous vous avons envoyé un lien de renitialisation`,
+      });
     } catch (error) {
-      response.status(403).json({message:"génération de lien de renitialisation de mot de passe echoué"})
+      response.status(403).json({
+        message: "génération de lien de renitialisation de mot de passe echoué",
+      });
     }
   })
 
-  .put("/:id",Auth, async (request, response) => {
-   
-    response.json({ user: await updateByID( request.params.id, request.body) });
+  .put("/:id", Auth, async (request, response) => {
+    response.json({ user: await updateByID(request.params.id, request.body) });
   })
-  
 
   .post("/", async (request, response) => {
-    const user = await createUser(request.body)
+    const user = await createUser(request.body);
     response.json({ user });
   })
 
-  .post("/login", async(request,response)=> {
+  .post("/login", async (request, response) => {
     try {
-      const user = await loginUser({email:request.body.email  , password:request.body.password})
-      const token = generateToken(user.id,user.email)
-      response.json({token})
-    } 
-    catch(error:any) {
-      response.status(403).json({message:error.message})
-      
+      const user = await loginUser({
+        email: request.body.email,
+        password: request.body.password,
+      });
+      const token = generateToken(user.id, user.email);
+      response.json({ token });
+    } catch (error: any) {
+      response.status(403).json({ message: error.message });
     }
-
-  })
+  });
